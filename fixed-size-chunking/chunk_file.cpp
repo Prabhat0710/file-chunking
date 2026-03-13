91c3b65b52088b8e8d267baf1f48ca4f5c9c11c5
Prabhat0710 <prabhatsingh4073@gmail.com>
2026-03-13 23:42:38 +0530

    Implement chunkFile() function for fixed-size file chunking

--- fixed-size-chunking/chunk_file.cpp
+++ fixed-size-chunking/chunk_file.cpp
@@ -3,11 +3,19 @@
 #include <vector>
 using namespace std;
 
+//an infiinte loop for the program 
+//give user 2 option chunking or merging
+//if chunking, provide file path, file shoujld be there
+//if merging, then ask for file name and if a folder with file name is found inside the  chunks_folder then start assembling the file
 int main(){
-    string inputFile = "input.txt";
-    int chunkSize = 4;
+    string inputFile = "input.txt";//make this path dynamic
+    // string inputFile = "../files/input.txt";//make this path dynamic
 
+    int chunkSize = 4;//take chunks size from user
+
+    //opens the user specfied file and puts it in the file variable
     ifstream file(inputFile, ios::binary);
+
 
     if(!file){
         cout << "Error opening file" << endl;
@@ -24,13 +32,13 @@ int main(){
         // file.read(buffer.data(), chunkSize) this tells to read 4 bytes from the file and store it in buffer
         // buffer.data() will give the first address of beffer cell
         // file.gcount() if last chunk contains less than chunkSize of bytes it will handle that
-        string outFile = "chunk_" + to_string(chunkNumber) + ".txt";
-        ofstream chunkFile(outFile, ios::binary);
+        string outFile = "chunk_" + to_string(chunkNumber) + ".txt";//this nameing will be dyanamic and tthe prefix will be the name of the file openeed
+        ofstream chunkFile(outFile, ios::binary);//the chunk extension should be dynamic
         chunkFile.write(buffer.data(), file.gcount());
         chunkFile.close();
         chunkNumber++;
     }
-
+    //whenever a new file is chunked createcx a new folder inside the chunks folder and dynamically add a folder inside that foler with files's name and all chunks of thatt file should be placed inside that folder
     file.close();
     cout << "File chunks successfully created." << endl;
     return 0;
